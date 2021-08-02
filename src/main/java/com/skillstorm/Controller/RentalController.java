8572d025ab3be2c7e00e1751832dfcd7c2a18258
mustafamanya <41771186+mustafamanya@users.noreply.github.com>
2021-08-02 11:11:52 -0700

    finished create in html
    finished adding form and java script for create functionality. Added some bootstrap but still needs lots of work.

--- src/main/java/com/skillstorm/Controller/RentalController.java
+++ src/main/java/com/skillstorm/Controller/RentalController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.Renderer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skillstorm.data.RentalDAO;
@@ -29,6 +30,7 @@ public class RentalController extends HttpServlet {
 		
 		
 		if(req.getParameter("id") != null) {
+			
 		String param= req.getParameter("id");
 		int id= Integer.parseInt(param);
 		Rental renter= dao.Findbooking(id);
